c4fe757215913e3de964ddf045824fb39609fc83
AdrienJ0 <adrientchuemtchuente@gmail.com>
2023-03-01 19:13:43 +0100

    update

---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TimerScript : MonoBehaviour
+{
+
+    private float time;
+    private float timeMax = 30;
+    public float TimerInterval;
+    private float tick;
+    [SerializeField] private TextMeshProUGUI timerText;
+
+    public GameObject scoreCanvas;
+    public GameObject timeOverPanel;
+    public GameObject endScorePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //time = (float)Time.time;
+        timerText.color = Color.white;
+        timerText.text = string.Format("{0:0.00}", timeMax);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time = (float)Time.time;
+        if (timeMax - time > 0)
+        {
+            if(timeMax - time >= 10)
+            {
+                timerText.color = Color.white;
+                timerText.text = string.Format("{0:0.00}", timeMax - time);
+            }
+            else
+            {
+                timerText.color = Color.red;
+                timerText.text = string.Format("{0:0.00}", timeMax - time);
+            }
+            
+            
+        }
+        if(timeMax - time <= 0)
+        {
+            timerText.text = "0.00";
+            ShowTimeOut();
+            ShowEndScore();
+        }
+
+        
+    }
+
+    void ShowTimeOut()
+    {
+        scoreCanvas.SetActive(false);
+        timeOverPanel.SetActive(true);
+        Time.timeScale = 0;
+        //yield return new WaitForSeconds(2f);
+    }
+
+    void ShowEndScore()
+    {
+        Time.timeScale = 0;
+        timeOverPanel.SetActive(false);
+        endScorePanel.SetActive(true);
+    }
+}
