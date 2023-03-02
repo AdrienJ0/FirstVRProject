78fb430ae36c6fddb719cf3ad61a4280b5ea27ad
LANDRY-SANON <landrysanou11@gmail.com>
2023-03-02 09:34:10 +0100

    LastV

--- Assets/LoadBack.cs
+++ Assets/LoadBack.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class LoadBack : MonoBehaviour
 {
-    public void LoadScene(string sceneName)
+    public void LoadSBack(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
